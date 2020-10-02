cb3d83154b37ab19530f10944eeed1e5b09db255
Arman <inzamam.arman@gmail.com>
2020-10-02 19:51:21 +0600

    gettong products related to category

--- controllers/product.js
+++ controllers/product.js
@@ -197,7 +197,33 @@ exports.list = (req,res,next)=>{
                 });
             }
             
-            res.send(products);
+            res.json(products);
 
         });
 };
+
+
+/**
+ * it will find the products based on the req product category
+ * other products that has the same category, will be returned
+ */
+
+
+exports.listRelated=(req,res,next)=>{
+    let limit = req.query.limit ? parseInt(req.query.limit):6;
+
+    Product.find({_id: {$ne:req.product}, category: req.product.category})
+    .limit(limit)
+    .populate('category','_id name')
+    .exec((err,products)=>{
+        res.setHeader('Content-Type','application/json');
+        if(err){
+            return res.status(400).json({
+                error: errorHandler(err),
+                message:'Product not found'
+            });
+        }
+
+        res.status(200).json(products);
+    })
+}

21d114839d35af5563cf54d06e818331ee87fdd4
Arman <inzamam.arman@gmail.com>
2020-10-31 17:08:39 +0600

    order put route and updating status of orders

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,25 @@
 const { Order, CartItem } =require('../models/order');
 const { errorHandler } = require('../helpers/dbErrorHandler');
+const { orderBy } = require('lodash');
+
+
+
+
+exports.orderById = (req,res,next,id)=>{
+    Order.findById(id)
+    .populate("products.product","name price") // only populate product's name aand price
+    .exec((error,order)=>{
+        if(error || !order){
+            return res.status(400).json({
+                error: errorHandler(error)
+            })
+        }
+
+        req.order= order; // this will availabe to alll the order routes and we can find the order detail
+        next();
+    })
+}
+
 
 exports.create = (req,res)=>{
  //   console.log("req.profile ",req.profile);
@@ -19,17 +39,35 @@ exports.create = (req,res)=>{
 
 }
 
-
-exports.listOrders = (req,res)=>{
+exports.listOrders = (req, res) => {
     Order.find()
-    .populate("user","_id name address")
-    .sort("-created")
-    .exec((err,orders)=>{
-        if(err){
+        .populate('user', '_id name address')
+        .sort('-created')
+        .exec((err, orders) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(error)
+                });
+            }
+            res.json(orders);
+        });
+};
+
+
+
+exports.getStatusValues = (req,res)=>{
+        //console.log("enum  val ",CartItem.schema.path("product").productValues)
+    return res.json(Order.schema.path("status").enumValues)
+};
+
+
+exports.updateOrderStatus = (req,res)=>{
+    Order.update({_id: req.body.orderId}, {$set:{status: req.body.status}} , (error,order)=>{
+        if (err) {
             return res.status(400).json({
                 error: errorHandler(error)
-            })
+            });
         }
-        res.status(200).json(orders);
+        res.json(order);
     })
 }
